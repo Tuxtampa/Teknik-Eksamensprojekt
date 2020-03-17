ee82a3a1e0ea3d05c2cfef516df80f7e8b461946
Tuxtampa <37031351+Tuxtampa@users.noreply.github.com>
2020-03-17 08:55:14 +0100

    quick

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@ import processing.core.PApplet;
 
 public class Main extends PApplet {
     public static void main(String[] args) {
-        //nice
+    }
+
+    public void draw(){
+        background(200);
+
     }
 }
