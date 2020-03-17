edf660f9cf34d0c8d33a8f7070f123c54eac3ee9
Biggie Cheese <50361292+GitHubMaster69@users.noreply.github.com>
2020-03-17 10:29:53 +0100

    Class related stuff fix

--- src/main/java/Board.java
+++ src/main/java/Board.java
@@ -1,9 +1,13 @@
 public class Board {
-    private Unit[][] units = new Unit[8][8];
+    private Tile[][] tiles = new Tile[8][8];
+
 
     public Board()  {
-
-
+        for(int i = 0; i < tiles.length; i++) {
+            for(int u = 0; u < tiles.length; u++) {
+                this.tiles[i][u] = new Tile(i,u);
+            }
+        }
     }
 
 
---
+++ src/main/java/Tile.java
@@ -0,0 +1,15 @@
+public class Tile {
+    int x;
+    int y;
+    Unit unit;
+
+    public Tile(int x, int y)   {
+        this.x = x;
+        this.y = y;
+        Unit = null;
+    }
+
+
+
+
+}
