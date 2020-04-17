fb561f4df123061775d91f4901388f813066be3f
Tuxtampa <37031351+Tuxtampa@users.noreply.github.com>
2020-04-17 17:51:17 +0200

    Not-quite-finished navigation
    They now don't go into squares that they know already have units, problems is i never tell the squares which of them have units

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,45 +3,83 @@ import processing.core.PImage;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class Main extends PApplet {
     public static void main(String[] args) {
         Main.main("Main");
     }
     ArrayList<Unit> units = new ArrayList<Unit>();
+    ArrayList<Unit> unitsTeam1 = new ArrayList<Unit>();
+    ArrayList<Unit> unitsTeam2 = new ArrayList<Unit>();
+    ArrayList<Unit> unitsTeam1InPlay = new ArrayList<Unit>();
+    ArrayList<Unit> unitsTeam2InPlay = new ArrayList<Unit>();
+    public HashMap<String,String> tiles = new HashMap<>();
     Board bd = new Board();
     int millisPerTick = 1000;
     int lastTick = 0;
+    float xTiles = 8;
+    float yTiles = 8;
     PImage unitImage = null;
 
     public void draw(){
         //background(200);
         bd.Boardd();
+        tileLines();
         //if((millis() - lastTick) > millisPerTick)tick();
     }
 
+    private void tileLines() {
+        for(int i = 1; i < xTiles; i++) {
+            line(width / xTiles*i, 0, width / xTiles*i, height);
+            line(0, 0+(height/yTiles*i), width, 0+(height/yTiles*i));
+        }
+    }
+
     public void tick(){
-        System.out.println("tick" + frameCount%(10*frameRate) + " There are " + units.size() + " units");
-        for(Unit currentUnit : units){
-            Unit target = getNearbyUnits(currentUnit.posX, currentUnit.posY);
-            if(target != null){
-                System.out.println("move");
-                currentUnit.nextMove(target);
+        System.out.println("tick" + frameCount%(10*frameRate) + " There are " + (unitsTeam1.size()+unitsTeam2.size()) + " units at ");
+        for(Unit currentUnit : unitsTeam1InPlay){
+            if(currentUnit.currentHp < 0) unitsTeam1InPlay.remove(currentUnit);
+            if(currentUnit.ticksToNextMove < 1) {
+                Unit target = getNearbyUnits(currentUnit);
+                if (target != null) {
+                    System.out.println("move");
+                    currentUnit.nextMove(target, tiles);
+                }
+            }
+        }
+        for(Unit currentUnit : unitsTeam2InPlay){
+            if(currentUnit.currentHp < 0) unitsTeam2InPlay.remove(currentUnit);
+            if(currentUnit.ticksToNextMove < 1) {
+                Unit target = getNearbyUnits(currentUnit);
+                if (target != null) {
+                    System.out.println("move");
+                    currentUnit.nextMove(target, tiles);
+                }
             }
         }
         lastTick = millis();
+        unitsTeam1InPlay.removeIf(currentUnit -> currentUnit.currentHp < 0.1);
+        unitsTeam2InPlay.removeIf(currentUnit -> currentUnit.currentHp < 0.1);
         drawUnits();
-        System.out.println(units.get(0).currentHp);
     }
 
-    public Unit getNearbyUnits(int startX, int startY){
-            for(int i = 0; i < 100; i++){
-                Unit tempUnit = units.get((int) Math.floor(Math.random()*units.size()));
-                if(tempUnit.posX != startX || tempUnit.posY != startY){
-                    return tempUnit;
+    public Unit getNearbyUnits(Unit currentunit){
+        try {
+            for (int i = 0; i < 100; i++) {
+                Unit tempUnit = unitsTeam1InPlay.get((int) Math.floor(Math.random() * units.size()));
+                if (tempUnit.posX != currentunit.posX || tempUnit.posY != currentunit.posY) {
+                    if (tempUnit.team != currentunit.team) return tempUnit;
+                }
+                Unit tempUnit2 = unitsTeam2InPlay.get((int) Math.floor(Math.random() * units.size()));
+                if (tempUnit2.posX != currentunit.posX || tempUnit2.posY != currentunit.posY) {
+                    if (tempUnit2.team != currentunit.team) return tempUnit2;
                 }
             }
             return null;
+        } catch (IndexOutOfBoundsException ie){
+            return null;
+        }
     }
 
     public void settings() {
@@ -50,45 +88,66 @@ public class Main extends PApplet {
 
     public void setup(){
         loadImages();
-        createUnit();
-        loadUnits();
+        fillTiles();
         System.out.println("Setup ran");
     }
 
     public void drawUnits(){
-        for(Unit currentUnit : units){
-            image(unitImage,currentUnit.posX,currentUnit.posY);
+        background(125);
+        tileLines();
+        for(Unit currentUnit : unitsTeam1InPlay){
+            image(unitImage,currentUnit.posX*(width/xTiles),currentUnit.posY*(height/yTiles));
+        }
+        for(Unit currentUnit : unitsTeam2InPlay){
+            image(unitImage,currentUnit.posX*(width/xTiles),currentUnit.posY*(height/yTiles));
         }
     }
-    public void mouseClicked(){
-        units.add(unitFromParent(units.get(units.size()-1)));
-        System.out.println(Arrays.toString(units.get(units.size() - 1).statWeights));
-        System.out.println(units.get(units.size() - 1).range);
-        System.out.println(units.get(units.size() - 1).attackSpeed);
-        System.out.println(units.get(units.size() - 1).hp);
-        System.out.println(units.get(units.size() - 1).damage);
-    }
+
     public void keyPressed(){
         if(key == 't')tick();
+        if(key == 'l'){
+            loadUnits();
+            drawUnits();
+        }
+        if(key == '1')createUnit(1);
+        if(key == '2')createUnit(2);
     }
 
     public void loadImages(){
         unitImage = loadImage("Images\\placeholder.PNG");
     }
 
-    public void createUnit() {
+    public void createUnit(int team) {
         for (int i = 0; i < 1; i++) {
             Unit unit2 = new Unit();
             unit2.UnitGeneric();
-            units.add(unit2);
+            if(team == 1)unitsTeam1.add(unit2);
+            if(team == 2)unitsTeam2.add(unit2);
         }
+        System.out.print("Unit created for team " + team);
     }
     public void loadUnits(){
-        for(int i = 0; i < units.size(); i++){
-            Unit currentUnit = units.get(i);
-            currentUnit.loadUnit(100,100*i);
+        for(int i = 0; i < unitsTeam1.size(); i++){
+            Unit currentUnit = unitsTeam1.get(i);
+            currentUnit.loadUnit(i+1,1 );
+            unitsTeam1InPlay.add(currentUnit);
+        }
+        for(int i = 0; i < unitsTeam2.size(); i++){
+            Unit currentUnit = unitsTeam2.get(i);
+            currentUnit.loadUnit(i+1,7 );
+            unitsTeam2InPlay.add(currentUnit);
+            tiles.put(currentUnit.posX +  "," + currentUnit.posY, "1");
         }
     }
+
+    public void fillTiles(){
+        for(int i = 0; i < 8; i++){
+            for(int j = 0; j < 8; j++){
+                tiles.put(j + "," + i, "0");
+            }
+        }
+    }
+
     public Unit unitFromParent(Unit parent){
         Unit newUnit = new Unit();
         newUnit.UnitWeights(parent.geneticAlgorithm(parent.statWeights));
--- src/main/java/Unit.java
+++ src/main/java/Unit.java
@@ -1,5 +1,7 @@
-import static java.lang.Integer.signum;
+import java.util.HashMap;
+
 import static java.lang.StrictMath.round;
+import static java.lang.StrictMath.signum;
 
 public class Unit {
     //Baseline stats that all units will need, as of now the idea is that extra stats might just be in the constructor,
@@ -10,8 +12,10 @@ public class Unit {
     float damage;
     float attackSpeed;            //hundredths of a second just to keep it an integer, might be stupid but whatever.
     int range = 1;
-    int posX = -1;
-    int posY = -1;
+    int posX = 1;
+    int posY = 1;
+    int team;
+    int ticksToNextMove = 0;
     float[] statWeights = {1,1,1,1};  //statWeights[0] = hp, statWeights[1] = damage, statWeights[2] = attackSpeed, statWeights[3] = range.
 
     void UnitGeneric(){
@@ -28,24 +32,66 @@ public class Unit {
         statWeights = importedWeights;
     }
 
-    void loadUnit(int newPosX, int newPosY){
-        posX = newPosX;
+    void loadUnit(int newPosY, int newTeam){
+        team = newTeam;
+        posX = team;
         posY = newPosY;
         currentHp = hp;
     }
 
-    void nextMove(Unit target){
+    void nextMove(Unit target, HashMap<String,String> tiles){
+        int tempTeam = (int) signum(team-2);
         if((-1-range) < posX-target.posX && posX-target.posX < (range+1) && (-1-range) < posY-target.posY && posY-target.posY < (range+1)){
-            System.out.println("attacking");
+            System.out.println(posX + " " + posY + " attacking " + target.posX + " " + target.posY + "from " + target.currentHp + " to ");
             target.currentHp -= damage();
+            System.out.println(target.currentHp);
             //attackAnimation(posX, posY, target.posX, target.posY);    TODO
         } else {
-            System.out.println("moving");
-            posX += signum(target.posX);
-            posY += signum(target.posY);
+            if(posY < target.posY){
+                topFree(tiles, tempTeam);
+                midFree(tiles, tempTeam);
+                downFree(tiles, tempTeam);
+            } else if (posY == target.posY){
+                midFree(tiles, tempTeam);
+                if (posY > 4){
+                    topFree(tiles, tempTeam);
+                } else {
+                    downFree(tiles, tempTeam);
+                }
+            } else {
+                downFree(tiles, tempTeam);
+                midFree(tiles, tempTeam);
+                topFree(tiles, tempTeam);
+            }
+                }
+            System.out.println(posX + " " + posY);
             //moveAnimation(posX, posY, signum(target.posX), signum(target.posY); TODO
         }
-}
+
+    void topFree(HashMap<String,String> tiles, int tempTeam) {
+        int tempPosX = posX-tempTeam;
+        int tempPosY = posY-1;
+        if (tiles.get(tempPosX + "," + tempPosY).equals("0")){
+            posY = tempPosY;
+            posX = tempPosX;
+        }
+    }
+    void midFree(HashMap<String,String> tiles, int tempTeam) {
+        int tempPosX = posX-tempTeam;
+        int tempPosY = posY;
+        if (tiles.get(tempPosX + "," + tempPosY).equals("0")){
+            posY = tempPosY;
+            posX = tempPosX;
+        }
+    }
+    void downFree(HashMap<String,String> tiles, int tempTeam) {
+        int tempPosX = posX-tempTeam;
+        int tempPosY = posY+1;
+        if (tiles.get(tempPosX + "," + tempPosY).equals("0")){
+            posY = tempPosY;
+            posX = tempPosX;
+        }
+    }
 
     float damage(){
         return damage;
